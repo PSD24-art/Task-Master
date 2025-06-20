d059af490f5ff5b76d4d4d21156cbafd43256038
Prathamesh <pratham.dahakepsd@gmail.com>
2025-06-20 16:33:03 +0530

    added edit functionalty prtially

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let title = document.getElementById("task-title");
-let decsript = document.getElementById("description");
+let descript = document.getElementById("description");
 
 let tasks = []; //for storing all tasks inside this array
 
@@ -7,54 +7,93 @@ submit.addEventListener("click", addTask);
 
 function addTask() {
   let taskTitle = title.value.trim();
-  let taskDescrpt = decsript.value.trim();
-  if (taskTitle === "") {
+  let taskDescrpt = descript.value.trim();
+  if (!taskTitle) {
     alert("Title is compulsory");
+    return;
   }
 
+  //Creating a new Object tasks
   const task = {
-    id: Date.now(),
     title: taskTitle,
     description: taskDescrpt,
   };
 
+  //Pushing the task to the Tasks array
   tasks.push(task);
 
+  //callback function
   renderTask();
+
+  //empty all the input fields
   title.value = "";
-  description.value = "";
+  descript.value = "";
   tasks = [];
   console.log(`Current Task: ${task.title}`);
 }
 
 function renderTask() {
   const card = document.getElementById("card");
+  const sepCard = document.createElement("div");
+  sepCard.setAttribute("class", "sepCard");
+  sepCard.classList.add("sepCard");
 
   tasks.forEach((item, index) => {
     //Title
-    const tTitle = document.createElement("p");
+    let tTitle = document.createElement("p");
     tTitle.innerText = item.title;
     tTitle.classList.add("tCard");
-    card.appendChild(tTitle);
+    sepCard.appendChild(tTitle);
 
     //Description
-    const tDecsript = document.createElement("p");
-    tDecsript.innerText = item.description;
-    tDecsript.classList.add("tCard");
-    card.appendChild(tDecsript);
+    let tDescript = document.createElement("p");
+    tDescript.innerText = item.description;
+    tDescript.classList.add("tCard");
+    sepCard.appendChild(tDescript);
 
-    //Delete button
+    //Button group div
+    const div = document.createElement("div");
+    div.setAttribute("class", "btnGrp");
+    // div.classList.add("btnGrp");
+
+    //adding delete button inside div
     const delBtn = document.createElement("button");
     delBtn.innerText = "Delete";
-    card.appendChild(delBtn);
+    div.appendChild(delBtn);
     delBtn.classList.add("btn");
 
-    //Event handler
+    //Event handler Delete
     delBtn.addEventListener("click", () => {
       tasks.splice(index, 1);
       renderTask();
-      card.innerHTML = "";
+      sepCard.innerHTML = "";
     });
-    console.log(tasks);
+
+    //Edit button
+    const editBtn = document.createElement("button");
+    editBtn.innerText = "Edit";
+    div.appendChild(editBtn);
+    editBtn.classList.add("btn");
+
+    //Event handler Edit
+    editBtn.addEventListener("click", () => {
+      //Creating new Input title and description
+      let titleInput = document.createElement("input");
+      let descriptInput = document.createElement("textarea");
+
+      //Assigning the existing values to the respective inputs
+      titleInput.value = item.title;
+      descriptInput.value = item.description;
+
+      //Updating the child element with new input elements
+      sepCard.replaceChild(titleInput, tTitle);
+      sepCard.replaceChild(descriptInput, tDescript);
+    });
+
+    //Adding div to sepCard and sepCard to card
+    sepCard.appendChild(div);
+    card.appendChild(sepCard);
   });
 }
+
+//Delete button
