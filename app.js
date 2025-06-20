399223bd94f136e7c98a4005f45cc8a6d6be1b89
Prathamesh <pratham.dahakepsd@gmail.com>
2025-06-20 11:50:43 +0530

    added delete functionality

--- app.js
+++ app.js
@@ -1,38 +1,60 @@
-let input = document.getElementById("task-title");
+let title = document.getElementById("task-title");
+let decsript = document.getElementById("description");
 
 let tasks = []; //for storing all tasks inside this array
 
 submit.addEventListener("click", addTask);
 
 function addTask() {
-  let taskText = input.value.trim();
-  if (taskText === "") {
+  let taskTitle = title.value.trim();
+  let taskDescrpt = decsript.value.trim();
+  if (taskTitle === "") {
     alert("Title is compulsory");
-    return;
   }
 
   const task = {
     id: Date.now(),
-    text: taskText,
+    title: taskTitle,
+    description: taskDescrpt,
   };
 
   tasks.push(task);
-  input.value = " ";
 
   renderTask();
-  input.value = "";
+  title.value = "";
+  description.value = "";
   tasks = [];
-
-  console.log(`Current Task: ${task.text}`);
+  console.log(`Current Task: ${task.title}`);
 }
 
 function renderTask() {
-  const list = document.getElementById("card");
+  const card = document.getElementById("card");
 
-  tasks.forEach((item) => {
-    const tCard = document.createElement("div");
-    tCard.innerText = item.text;
-    tCard.classList.add("tCard");
-    list.appendChild(tCard);
+  tasks.forEach((item, index) => {
+    //Title
+    const tTitle = document.createElement("p");
+    tTitle.innerText = item.title;
+    tTitle.classList.add("tCard");
+    card.appendChild(tTitle);
+
+    //Description
+    const tDecsript = document.createElement("p");
+    tDecsript.innerText = item.description;
+    tDecsript.classList.add("tCard");
+    card.appendChild(tDecsript);
+
+    //Delete button
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    card.appendChild(delBtn);
+    delBtn.classList.add("btn");
+
+    //Event handler
+    delBtn.addEventListener("click", () => {
+      tasks.splice(index, 1);
+      renderTask();
+      card.innerHTML = "";
+    });
+    console.log(tasks);
   });
 }
